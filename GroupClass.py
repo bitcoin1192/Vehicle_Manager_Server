2957e1c5b9d10c2a3dfbeef0d9e90109fce7f2d8
Ariq <bitcoin1192@gmail.com>
2022-03-23 15:33:32 +0700

    Fix sql script and letting user change multiple member addition or deletion

--- GroupClass.py
+++ GroupClass.py
@@ -1,6 +1,6 @@
 import sqlite3
 from UserClass import UserNotFound
-from main import UnknownIntent
+from ControllerException import UnknownIntent
 
 class GroupClass:
     latest_response = None
@@ -8,33 +8,37 @@ class GroupClass:
         self.msg = msg
         self.sqlConn = conn
         self.curr = self.sqlConn.cursor()
-        sqlScript = self.curr.execute("SELECT MSTblUserLogin.UID, GID FROM GIDHEADER INNER JOIN MSTblUserLogin on MSTblUserLogin.UID = GIDHeader.UIDOwner")
+        sqlScript = self.curr.execute("""SELECT MSTblUserLogin.UID, GID FROM GIDHEADER 
+                                        INNER JOIN MSTblUserLogin on MSTblUserLogin.UID = GIDHeader.UIDOwner""")
         result = sqlScript.fetchone()
         self.uid = uid
         self.groupID = result[1]
 
     def intentReader(self):
-        self.vehicleID = self.msg["VID"]
-        self.friendUID = self.msg["UIDMember"]
         intent = self.msg["intent"]
-        if intent == "add":
-            self.addFriend()
-            self.latest_response = "Member is added to group"
-        elif intent == "Delete":
-            self.removeFriend()
-            self.latest_response = "Member is deleted from group"
-        else:
-            raise UnknownIntent
+        groupMember = self.msg["changeMember"]
+        for each in groupMember:
+            if intent == "add":
+                self.addFriend(each["VID"],each["UID"])
+                self.latest_response = "Member is added to group"
+            elif intent == "delete":
+                self.removeFriend(each["UID"])
+                self.latest_response = "Member is deleted from group"
+            else:
+                raise UnknownIntent
+        self.sqlConn.commit()
         
-    def addFriend(self):
+    def addFriend(self, VID, UIDMember):
         try:
-            self.curr.execute("INSERT INTO TRGIDMember (GID,VID,UIDMember) values (:GID,:VID,:UIDMember)",{"GID": self.groupID,"VID":self.vehicleID,"UIDMember":self.friendUID})
-            self.sqlConn.commit()
+            self.curr.execute("""INSERT INTO TRGIDMember (GID,VIDLease,UIDMember)
+                                 values (:GID,:VID,:UIDMember)""",
+                                 {"GID": self.groupID,"VID":VID,"UIDMember":UIDMember})
         except sqlite3.IntegrityError as e:
             raise UserNotFound("UIDMember is not found")
-    def removeFriend(self):
+    def removeFriend(self, UIDMember):
         try:
-            self.curr.execute("DELETE FROM TRGIDMember WHERE UIDMember=:UIDMember",{"UIDMember":self.friendUID})
-            self.sqlConn.commit()
+            self.curr.execute("""DELETE FROM TRGIDMember
+                                 WHERE UIDMember=:UIDMember and GID=:GID""",
+                                 {"UIDMember":UIDMember,"GID":self.groupID})
         except sqlite3.OperationalError as e:
             raise UserNotFound("UIDMember is not found")
